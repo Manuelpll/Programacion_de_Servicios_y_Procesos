3bdc822a1d23b2e9e33e7fc6706670bc580bed02
= <=>
2025-01-20 19:08:01 +0100

    Primeros ejercicios del tema 4 y algunos restantes del tema 3

---
+++ src/U_3/Ejercicios_Clase/Apartado_3_8/Hilos/Servidor.java
@@ -0,0 +1,23 @@
+package U_3.Ejercicios_Clase.Apartado_3_8.Hilos;
+
+import java.io.*;
+import java.net.*;
+
+public class Servidor {
+	public static void main(String args[]) throws IOException {
+		ServerSocket serverSocket;
+		serverSocket = new ServerSocket(6000);
+		System.out.println("Servidor iniciado...");
+
+		while (true) {
+			Socket socket = new Socket();
+			socket = serverSocket.accept();// esperando cliente
+			HiloServidor hiloServidor = new HiloServidor(socket);
+			hiloServidor.start();
+		}
+	}
+}// ..
+
+
+
+
